60c8c25f3d6238ed4d01281dcde27c646967d9dd
Andrei Stanciu <andreistanciu.as@gmail.com>
2018-01-05 14:32:07 +0200

    Advent of Code 2017: Day6 part two finished

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -11,12 +11,16 @@ fn main() {
             .split_whitespace()
             .map(|word| word.trim().parse::<i32>().unwrap())
             .collect::<Vec<i32>>();
-        println!("Redistribution steps: {}", count_redis_steps(&list));
+        let (steps, cycle_list) = count_redis_steps(&list);
+        println!("Redistribution steps: {}", steps);
+        println!(
+            "Infinite loops cycle steps: {:?}",
+            count_cycles_infinite_loop((cycle_list.as_slice()))
+        );
     }
 }
 
-
-fn count_redis_steps(list: &[i32]) -> i32 {
+fn count_redis_steps(list: &[i32]) -> (i32, Vec<i32>) {
     // keep the first list in memory
     let mut curr_list = list.iter().cloned().collect::<Vec<_>>();
     let len = curr_list.len();
@@ -46,6 +50,40 @@ fn count_redis_steps(list: &[i32]) -> i32 {
         }
 
         if memory.contains(&curr_list) {
+            break;
+        } else {
+            no_steps += 1;
+        }
+    }
+
+    (no_steps, curr_list)
+}
+
+fn count_cycles_infinite_loop(list: &[i32]) -> i32 {
+    let mut curr_list = list.iter().cloned().collect::<Vec<_>>();
+    let len = curr_list.len();
+    let mut no_steps = 1;
+    loop {
+        if let Some((idx, &max_val)) = curr_list
+            .iter()
+            .enumerate()
+            .rev()
+            .max_by(|x, y| x.1.cmp(y.1))
+        {
+            let mut pos = idx;
+            let mut max = max_val;
+
+            // set the maximum value to zero
+            curr_list[idx] = 0;
+
+            while max > 0 {
+                curr_list[(pos + 1) % len] += 1;
+                max -= 1;
+                pos = (pos + 1) % len;
+            }
+        }
+
+        if curr_list == list {
             break;
         } else {
             no_steps += 1;
