9af1604cc0ef34efd21414c477e0ac137e638c1d
Andrei Stanciu <andreistanciu.as@gmail.com>
2018-01-17 23:12:02 +0200

    Advent of Code 2017: Day15 part one done

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,19 +1,39 @@
 fn main() {
-    let mut prev_val_a = 65;
-    let mut prev_val_b = 8921;
+    let mut prev_val_a = 618;
+    let mut prev_val_b = 814;
     let factor_a = 16807;
     let factor_b = 48271;
 
-    let mut iterations = Vec::new();
-    for i in 0..5 {
-        prev_val_a = generate_val(prev_val_a, factor_a);
-        prev_val_b = generate_val(prev_val_b, factor_b);
-        iterations.push((prev_val_a, prev_val_b));
-    }
-    println!("{:?}", iterations);
+    println!(
+        "{:?}",
+        count_matches(
+            40_000_000,
+            &mut prev_val_a,
+            &mut prev_val_b,
+            factor_a,
+            factor_b
+        )
+    );
 }
 
+fn count_matches(
+    iterations: u64,
+    prev_val_a: &mut u64,
+    prev_val_b: &mut u64,
+    factor_a: u64,
+    factor_b: u64,
+) -> u64 {
+    let mut count = 0;
+    for _ in 0..iterations {
+        *prev_val_a = generate_val(*prev_val_a, factor_a);
+        *prev_val_b = generate_val(*prev_val_b, factor_b);
+
+        if *prev_val_a & <u16>::max_value() as u64 == *prev_val_b & <u16>::max_value() as u64 {
+            count += 1;
+        }
+    }
+    count
+}
 fn generate_val(prev_val: u64, factor: u64) -> u64 {
     prev_val * factor % 2147483647
 }
-
