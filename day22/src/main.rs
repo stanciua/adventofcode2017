ad3ecbffefd28cb0f4aafa7411ea356c3630dc6f
Andrei Stanciu <andreistanciu.as@gmail.com>
2018-03-01 14:35:59 +0200

    Advent of Code 2017: Day22 part two finished

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -9,6 +9,19 @@ enum Direction {
     Right,
 }
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Flag {
+    Clean,
+    Flagged,
+    Infected,
+    Weakened,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Algorithm {
+    V1,
+    V2,
+}
 fn main() {
     let path = "input.txt";
     let mut input = File::open(path).expect("Unable to open file!");
@@ -26,7 +39,23 @@ fn main() {
     println!(
         "The number of infections after {} bursts is: {}",
         10_000,
-        move_virus(initial_grid.as_slice(), initial_grid[0].len(), 10_000)
+        advance(
+            Algorithm::V1,
+            initial_grid.as_slice(),
+            initial_grid[0].len(),
+            10_000
+        )
+    );
+
+    println!(
+        "The number of infections after {} bursts is: {}",
+        10_000_000,
+        advance(
+            Algorithm::V2,
+            initial_grid.as_slice(),
+            initial_grid[0].len(),
+            10_000_000
+        )
     );
 }
 
@@ -51,13 +80,11 @@ fn extend_grid(start_grid: &[Vec<char>], size: usize) -> Vec<Vec<char>> {
 fn translate_point(point: (usize, usize), from: usize, to: usize) -> (usize, usize) {
     let middle_from = (from / 2, from / 2);
     let middle_to = (to / 2, to / 2);
-
     let up_left = (middle_to.0 - middle_from.0, middle_to.1 - middle_from.1);
-
     (up_left.0 + point.0, up_left.1 + point.1)
 }
 
-fn move_virus(from_grid: &[Vec<char>], size: usize, bursts: usize) -> usize {
+fn advance(algorithm: Algorithm, from_grid: &[Vec<char>], size: usize, bursts: usize) -> usize {
     let mut curr_dir = Direction::Up;
     let mut grid = extend_grid(from_grid, size);
     let mut curr_pos = (size / 2, size / 2);
@@ -70,21 +97,39 @@ fn move_virus(from_grid: &[Vec<char>], size: usize, bursts: usize) -> usize {
         if curr_pos.0 == 0 || curr_pos.0 == grid_size - 1 || curr_pos.1 == 0
             || curr_pos.1 == grid_size - 1
         {
-            grid = extend_grid(grid.as_slice(), grid_size * 2);
-            curr_pos = translate_point(curr_pos, grid_size, grid_size * 2);
+            grid = extend_grid(grid.as_slice(), grid_size + 2);
+            curr_pos = translate_point(curr_pos, grid_size, grid_size + 2);
         }
 
-        let mut infected = grid[curr_pos.0][curr_pos.1] == '#';
-        let (mut next_pos, next_dir) = get_next_pos(curr_pos, curr_dir, infected);
+        let mut state = match grid[curr_pos.0][curr_pos.1] {
+            '#' => Flag::Infected,
+            '.' => Flag::Clean,
+            'W' => Flag::Weakened,
+            'F' => Flag::Flagged,
+            _ => panic!("invalid value detected!"),
+        };
+        let (mut next_pos, next_dir) = get_next_pos(curr_pos, curr_dir, state);
         curr_dir = next_dir;
 
-        if infected {
-            grid[curr_pos.0][curr_pos.1] = '.';
-        } else {
-            grid[curr_pos.0][curr_pos.1] = '#';
-            infection_count += 1;
-        }
+        match state {
+            Flag::Clean => if algorithm == Algorithm::V1 {
+                grid[curr_pos.0][curr_pos.1] = '#';
+                infection_count += 1;
+            } else {
+                grid[curr_pos.0][curr_pos.1] = 'W'
+            },
+            Flag::Flagged => grid[curr_pos.0][curr_pos.1] = '.',
+            Flag::Infected => if algorithm == Algorithm::V1 {
+                grid[curr_pos.0][curr_pos.1] = '.'
+            } else {
+                grid[curr_pos.0][curr_pos.1] = 'F'
+            },
 
+            Flag::Weakened => {
+                grid[curr_pos.0][curr_pos.1] = '#';
+                infection_count += 1;
+            }
+        }
         curr_pos = next_pos;
     }
 
@@ -94,32 +139,35 @@ fn move_virus(from_grid: &[Vec<char>], size: usize, bursts: usize) -> usize {
 fn get_next_pos(
     curr_pos: (usize, usize),
     curr_dir: Direction,
-    infected: bool,
+    state: Flag,
 ) -> ((usize, usize), Direction) {
     match curr_dir {
-        Direction::Up => if infected {
-            ((curr_pos.0, curr_pos.1 + 1), Direction::Right)
-        } else {
-            ((curr_pos.0, curr_pos.1 - 1), Direction::Left)
+        Direction::Up => match state {
+            Flag::Clean => ((curr_pos.0, curr_pos.1 - 1), Direction::Left),
+            Flag::Weakened => ((curr_pos.0 - 1, curr_pos.1), Direction::Up),
+            Flag::Infected => ((curr_pos.0, curr_pos.1 + 1), Direction::Right),
+            Flag::Flagged => ((curr_pos.0 + 1, curr_pos.1), Direction::Down),
         },
-        Direction::Down => if infected {
-            ((curr_pos.0, curr_pos.1 - 1), Direction::Left)
-        } else {
-            ((curr_pos.0, curr_pos.1 + 1), Direction::Right)
+        Direction::Down => match state {
+            Flag::Clean => ((curr_pos.0, curr_pos.1 + 1), Direction::Right),
+            Flag::Weakened => ((curr_pos.0 + 1, curr_pos.1), Direction::Down),
+            Flag::Infected => ((curr_pos.0, curr_pos.1 - 1), Direction::Left),
+            Flag::Flagged => ((curr_pos.0 - 1, curr_pos.1), Direction::Up),
         },
-        Direction::Left => if infected {
-            ((curr_pos.0 - 1, curr_pos.1), Direction::Up)
-        } else {
-            ((curr_pos.0 + 1, curr_pos.1), Direction::Down)
+        Direction::Left => match state {
+            Flag::Clean => ((curr_pos.0 + 1, curr_pos.1), Direction::Down),
+            Flag::Weakened => ((curr_pos.0, curr_pos.1 - 1), Direction::Left),
+            Flag::Infected => ((curr_pos.0 - 1, curr_pos.1), Direction::Up),
+            Flag::Flagged => ((curr_pos.0, curr_pos.1 + 1), Direction::Right),
         },
-        Direction::Right => if infected {
-            ((curr_pos.0 + 1, curr_pos.1), Direction::Down)
-        } else {
-            ((curr_pos.0 - 1, curr_pos.1), Direction::Up)
+        Direction::Right => match state {
+            Flag::Clean => ((curr_pos.0 - 1, curr_pos.1), Direction::Up),
+            Flag::Weakened => ((curr_pos.0, curr_pos.1 + 1), Direction::Right),
+            Flag::Infected => ((curr_pos.0 + 1, curr_pos.1), Direction::Down),
+            Flag::Flagged => ((curr_pos.0, curr_pos.1 - 1), Direction::Left),
         },
     }
 }
-
 #[cfg(test)]
 mod test {
     use super::*;
