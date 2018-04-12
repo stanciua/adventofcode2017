62eaa8d30b2586e10b90ea117593d3a4cba09d2c
stanciua <andrei.stanciu@aciworldwide.com>
2018-04-12 13:58:18 +0300

    Advent of Code 2017: Day21, added flip_ud and flip_lr functions to perform matrix flips

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -2,9 +2,9 @@
 extern crate nom;
 
 use nom::*;
-use std::str;
 use std::fs::File;
 use std::io::Read;
+use std::str;
 
 fn main() {
     let path = "input.txt";
@@ -74,6 +74,114 @@ mod test {
             IResult::Done(&b""[..], vec![vec!['#', '#'], vec!['#', '#']])
         );
     }
+
+    #[test]
+    fn test_flip_ud_2d() {
+        let mut matrix = vec![vec![1, 2], vec![3, 4]];
+        flip_ud(matrix.as_mut_slice());
+        assert_eq!(matrix, vec![vec![3, 4], vec![1, 2]]);
+    }
+    #[test]
+    fn test_flip_ud_3d() {
+        let mut matrix = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        flip_ud(matrix.as_mut_slice());
+        assert_eq!(matrix, vec![vec![7, 8, 9], vec![4, 5, 6], vec![1, 2, 3]]);
+    }
+    #[test]
+    fn test_flip_ud_4d() {
+        let mut matrix = vec![
+            vec![1, 2, 3, 4],
+            vec![5, 6, 7, 8],
+            vec![9, 10, 11, 12],
+            vec![13, 14, 15, 16],
+        ];
+        flip_ud(matrix.as_mut_slice());
+        assert_eq!(
+            matrix,
+            vec![
+                vec![13, 14, 15, 16],
+                vec![9, 10, 11, 12],
+                vec![5, 6, 7, 8],
+                vec![1, 2, 3, 4],
+            ]
+        );
+    }
+    #[test]
+    fn test_flip_ud_5d() {
+        let mut matrix = vec![
+            vec![1, 2, 3, 4, 5],
+            vec![6, 7, 8, 9, 10],
+            vec![11, 12, 13, 14, 15],
+            vec![16, 17, 18, 19, 20],
+            vec![21, 22, 23, 24, 25],
+        ];
+        flip_ud(matrix.as_mut_slice());
+        assert_eq!(
+            matrix,
+            vec![
+                vec![21, 22, 23, 24, 25],
+                vec![16, 17, 18, 19, 20],
+                vec![11, 12, 13, 14, 15],
+                vec![6, 7, 8, 9, 10],
+                vec![1, 2, 3, 4, 5],
+            ]
+        );
+    }
+
+    // flip_lr tests
+    #[test]
+    fn test_flip_lr_2d() {
+        let mut matrix = vec![vec![1, 2], vec![3, 4]];
+        flip_lr(matrix.as_mut_slice());
+        assert_eq!(matrix, vec![vec![2, 1], vec![4, 3]]);
+    }
+    #[test]
+    fn test_flip_lr_3d() {
+        let mut matrix = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        flip_lr(matrix.as_mut_slice());
+        assert_eq!(matrix, vec![vec![3, 2, 1], vec![6, 5, 4], vec![9, 8, 7]]);
+    }
+    #[test]
+    fn test_flip_lr_4d() {
+        let mut matrix = vec![
+            vec![1, 2, 3, 4],
+            vec![5, 6, 7, 8],
+            vec![9, 10, 11, 12],
+            vec![13, 14, 15, 16],
+        ];
+        flip_lr(matrix.as_mut_slice());
+        assert_eq!(
+            matrix,
+            vec![
+                vec![4, 3, 2, 1],
+                vec![8, 7, 6, 5],
+                vec![12, 11, 10, 9],
+                vec![16, 15, 14, 13],
+            ]
+        );
+    }
+    #[test]
+    fn test_flip_lr_5d() {
+        let mut matrix = vec![
+            vec![1, 2, 3, 4, 5],
+            vec![6, 7, 8, 9, 10],
+            vec![11, 12, 13, 14, 15],
+            vec![16, 17, 18, 19, 20],
+            vec![21, 22, 23, 24, 25],
+        ];
+        flip_lr(matrix.as_mut_slice());
+        assert_eq!(
+            matrix,
+            vec![
+                vec![5, 4, 3, 2, 1],
+                vec![10, 9, 8, 7, 6],
+                vec![15, 14, 13, 12, 11],
+                vec![20, 19, 18, 17, 16],
+                vec![25, 24, 23, 22, 21],
+            ]
+        );
+    }
+
 }
 
 fn rotate_clockwise_90_deg(input: &[Vec<char>]) -> Vec<Vec<char>> {
@@ -84,4 +192,29 @@ fn rotate_clockwise_90_deg(input: &[Vec<char>]) -> Vec<Vec<char>> {
     // ->
     //
     output
+}
+
+fn flip_ud(matrix: &mut [Vec<i32>]) {
+    let lgth = matrix.len();
+    for idx in 0..lgth / 2 {
+        assert!(matrix[idx].len() == matrix[lgth - idx - 1].len());
+        let row_lgth = matrix[idx].len();
+        for i in 0..row_lgth {
+            let tmp = matrix[idx][i];
+            matrix[idx][i] = matrix[lgth - idx - 1][i];
+            matrix[lgth - idx - 1][i] = tmp;
+        }
+    }
+}
+
+fn flip_lr(matrix: &mut [Vec<i32>]) {
+    let lgth = matrix.len();
+    for idx in 0..lgth / 2 {
+        let row_lgth = matrix.len();
+        for i in 0..row_lgth {
+            let tmp = matrix[i][idx];
+            matrix[i][idx] = matrix[i][lgth - idx - 1];
+            matrix[i][lgth - idx - 1] = tmp;
+        }
+    }
 }
